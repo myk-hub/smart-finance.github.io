96ed5f2838683d4bcff07651bdf450cf386a5063
N7 <myk.hubal@gmail.com>
2020-11-14 19:32:52 +0200

    Updates

---
+++ precache-manifest.0c42829de8ae221ac68a249267b3d01a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c311730505f34559a57818bda0716306",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b39021fb3ade8916710",
+    "url": "/static/css/main.96826e9e.chunk.css"
+  },
+  {
+    "revision": "7e99b59efe01bb71214f",
+    "url": "/static/js/2.64c0745b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.64c0745b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b39021fb3ade8916710",
+    "url": "/static/js/main.f4e65591.chunk.js"
+  },
+  {
+    "revision": "d5e6d91bd6fc7052c6f5",
+    "url": "/static/js/runtime-main.93ca1d4e.js"
+  },
+  {
+    "revision": "dcd23bf86123bf4a57a2a80d579c6334",
+    "url": "/static/media/logo.dcd23bf8.svg"
+  }
+]);
